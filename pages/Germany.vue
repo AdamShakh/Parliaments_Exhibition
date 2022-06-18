49f56b7590b7dee195482453b41073b000cc065c
Adam <adamshah@yandex.ru>
2022-06-18 19:41:46 +0300

    Germany page

--- pages/Germany.vue
+++ pages/Germany.vue
@@ -1,14 +1,58 @@
 <template>
-<v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
+  <page-template :imgNum=this.imgNum :country=this.country :title=this.title>
 
+      <template  v-slot:Face>
+          <p style="margin: 0em; padding-top: 0.5em; padding-bottom: 0.5em">
+              Bundestag
+          </p>
+      </template>
 
-    </v-col>
-</v-row>
+      <template  v-slot:Left>
+          <p>
+            Nowadays Parliament of Germany - Bundestag - placed occupying several building in "Administrative district".
+            Among these buildings: 
+            Reichstag-building, the Jakob-Kaiser-Haus, Paul-Löbe-Haus and Marie-Elisabeth-Lüders-Haus.
+            Last two buildings with the Federal Chancellery are part of so called "Federal Ribbon".
+          </p>
+          <p>
+            Plenary sessions are held in the Reichstag building itself, and there is a permanent exhibition 
+            about the German parliament, with a beautiful public space under the dome. 
+            The offices of the deputies are located in the Jakob-Kaiser-Haus. 
+            In the Paul-Löbe-Haus there are committees, offices, and small meeting rooms. 
+            There is a library in the Elisabeth-Lüders-Haus.
+          </p>
+      </template>
+
+      <template  v-slot:Right>
+          <p> Bundestag (Federal Assembly) - Unicameral Federal Parliament. 
+            It is the only federal representative body that is directly elected by the German people.
+          </p>
+          <p>
+            It is comparable to the United States House of Representatives, 
+            the House of Commons of the United Kingdom 
+            and State Duma of Russian Federation.
+            It's one of the legislative bodies of Germany and thus it is the historical successor 
+            to the earlier Reichstag.
+          </p>
+      </template>
+
+  </page-template>
 </template>
 
 <script>
+import PageTemplate from '@/components/PageTemplate.vue'
+
 export default {
-  name: 'Germany'
+  name: 'Germany',
+  components:{
+    PageTemplate
+  },
+  data(){
+    return{
+      imgNum: 25,
+      country: "Germany",
+      title: "Bundestag"
+    }
+  }
 }
 </script>
